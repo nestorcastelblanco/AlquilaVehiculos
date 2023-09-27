9fbb512ea682c54a6f2458bcd2fcaaaddac04947
nestorcastelblanco <nestorfelipe10@gmail.com>
2023-09-26 20:41:53 -0500

    Update IngresoAdmin.java

--- src/main/java/co/edu/uniquindio/app/IngresoAdmin.java
+++ src/main/java/co/edu/uniquindio/app/IngresoAdmin.java
@@ -16,7 +16,7 @@ public class IngresoAdmin extends Application {
         stage.setTitle("INGRESO ADMINISTRADOR");
         stage.show();
     }
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
 
         launch( IngresoAdmin.class, args );
     }
